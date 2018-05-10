741563f7e50d9c61b3fe7d90e3b1f8db6d286bc2
Tibor Kocik <tibor.kocik@synapse.com>
2018-05-10 16:45:22 +0200

    Vytvoreny design pre zaznamenanie dochadzky

--- app/src/main/java/com/example/tiborkocik/camx_dochadzka/MainActivity.java
+++ app/src/main/java/com/example/tiborkocik/camx_dochadzka/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tiborkocik.camx_dochadzka;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +17,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    List<String> spinnerName;
+    List<String> spinnerReason;
+    List<String> spinnerTransport;
+    TextView time;
+    BroadcastReceiver _broadcastReceiver;
+    private final SimpleDateFormat _sdfWatchTime = new SimpleDateFormat("HH:mm");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +63,80 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        time = (TextView)findViewById(R.id.timeText);
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(500);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Calendar calendar = Calendar.getInstance();
+                                SimpleDateFormat mdformat = new SimpleDateFormat("HH:mm:ss");
+
+                                Calendar c = Calendar.getInstance();
+                                SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+                                String formatDate = df.format(c.getTime());
+
+                                String strDate = mdformat.format(calendar.getTime()) + "   " + formatDate;
+                                time.setText(strDate);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        t.start();
+
+
+
+        spinnerName =  new ArrayList<String>();
+        spinnerName.add("Martin Mrazko");
+        spinnerName.add("Tomas Granat");
+        spinnerName.add("Jakub Grega");
+        spinnerName.add("Tibor Kocik");
+
+        spinnerReason = new ArrayList<String>();
+        spinnerReason.add("Príchod");
+        spinnerReason.add("Odchod na obed");
+        spinnerReason.add("Príchod z obeda");
+        spinnerReason.add("Odchod");
+
+        spinnerTransport = new ArrayList<String>();
+        spinnerTransport.add("Neplatena");
+        spinnerTransport.add("Auto");
+        spinnerTransport.add("1x Autobus");
+        spinnerTransport.add("2x Autobus");
+
+
+
+
+        ArrayAdapter<String> adapterName = new ArrayAdapter<String>(
+                this, R.layout.spinner_layout, spinnerName);
+        adapterName.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItemsName = (Spinner) findViewById(R.id.nameSpinner);
+        sItemsName.setAdapter(adapterName);
+
+
+        ArrayAdapter<String> adapterReason = new ArrayAdapter<String>(
+                this, R.layout.spinner_layout, spinnerReason);
+        adapterReason.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItemsReason = (Spinner) findViewById(R.id.reasonSpinner);
+        sItemsReason.setAdapter(adapterReason);
+
+        ArrayAdapter<String> adapterTransport = new ArrayAdapter<String>(
+                this, R.layout.spinner_layout, spinnerTransport);
+        adapterReason.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItemsTransport = (Spinner) findViewById(R.id.transportSpinner);
+        sItemsTransport.setAdapter(adapterTransport);
     }
+
 
     @Override
     public void onBackPressed() {
